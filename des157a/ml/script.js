a04077eda753ab52641bfc28f577caf38ec22ff7
Evelyn Huey <evelynhuey@hotmail.com>
2021-10-19 00:10:24 -0700

    change script based on theme

--- des157a/ml/script.js
+++ des157a/ml/script.js
@@ -1,4 +1,5 @@
-(function() {
+
+(function(){
     "use strict";
     console.log("reading js");
 
@@ -7,25 +8,51 @@
 
     myForm.addEventListener("submit", function(event) {
         event.preventDefault();
-        let noun1 = document.querySelector("#noun1").value;
-        let noun2 = document.querySelector("#noun2").value;
+        let aTime = document.querySelector("#aTime").value;
         let adj = document.querySelector("#adj").value;
-        let verb = document.querySelector("#verb").value;
+        let aNum = document.querySelector("#aNum").value;
+        let color = document.querySelector("#color").value;
+        let direction = document.querySelector("#direction").value;
+        let tool = document.querySelector("#tool").value;
+        let item = document.querySelector("#item").value;
+        let decor = document.querySelector("#decor").value;
 
         let myText;
 
-        if (noun1 && noun2 && adj && verb) {
-            myText = `Here are the words: ${noun1}, ${noun2}, ${adj}, and ${verb}`;
+        if (aTime && adj && aNum && color && direction && tool && item && decor) {
+
+
+            myText = 
+            `Estimated time to build the table: ${aTime} <br>
+            <br>
+            Materials Needed:<br>
+            1 square piece of ${adj} wood <br>
+            4 table legs <br>
+            ${aNum} nails <br>
+            ${color} paint <br>
+            1 ${decor} <br>
+            <br>
+            Instructions: <br>
+            1. Put the square piece of wood ${direction} on the ground <br>
+            2. Nail each leg to the corners of the square wood using a ${tool} <br>
+            3. Sand the table with a piece of ${item} <br>
+            4. Paint the table with your ${color} paint <br>
+            5. Decorate your table with the ${decor} <br>`;
         } else {
             myText = "Please complete the form so I can make your Mad Lib!";
         }
 
         madLib.innerHTML = myText;
 
-        noun1 = document.querySelector("#noun1").value = "";
-        noun2 = document.querySelector("#noun2").value = "";
-        adj = document.querySelector("#adj").value = "";
-        verb = document.querySelector("#verb").value = "";
+        // reset the fields
+        // let aTime = document.querySelector("#aTime").value  = "";
+        // let adj = document.querySelector("#adj").value = "";
+        // let aNum = document.querySelector("#aNum").value = "";
+        // let color = document.querySelector("#color").value = "";
+        // let direction = document.querySelector("#direction").value = "";
+        // let tool = document.querySelector("#tool").value = "";
+        // let item = document.querySelector("#item").value = "";
+        // let decor = document.querySelector("#decor").value = "";
 
         // let formData = document.querySelectorAll("input[type=text");
         // for (let eachField of formData) {
