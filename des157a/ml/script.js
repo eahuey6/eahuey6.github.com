5d9cc9838c9115924f09f2ff9ca9c5dc49c919b7
eahuey6 <evelynhuey@hotmail.com>
2021-10-20 16:27:58 -0700

    wed studio

--- des157a/ml/script.js
+++ des157a/ml/script.js
@@ -1,5 +1,4 @@
-
-(function(){
+(function() {
     "use strict";
     console.log("reading js");
 
@@ -19,30 +18,44 @@
 
         let myText;
 
+        console.log("submit")
+        event.preventDefault();
+        document.getElementById("overlay").className = "showing";
+
         if (aTime && adj && aNum && color && direction && tool && item && decor) {
 
+            aTimeBlank.innerHTML = aTime;
+            adjBlank.innerHTML = adj;
+            aNumBlank.innerHTML = aNum;
+            colorBlank1.innerHTML = color;
+            colorBlank2.innerHTML = color;
+            directionBlank.innerHTML = direction;
+            toolBlank.innerHTML = tool;
+            itemBlank.innerHTML = item;
+            decorBlank1.innerHTML = decor;
+            decorBlank2.innerHTML = decor;
 
-            myText = 
-            `Estimated time to build the table: ${aTime} <br>
-            <br>
-            Materials Needed:<br>
-            1 square piece of ${adj} wood <br>
-            4 table legs <br>
-            ${aNum} nails <br>
-            ${color} paint <br>
-            1 ${decor} <br>
-            <br>
-            Instructions: <br>
-            1. Put the square piece of wood ${direction} on the ground <br>
-            2. Nail each leg to the corners of the square wood using a ${tool} <br>
-            3. Sand the table with a piece of ${item} <br>
-            4. Paint the table with your ${color} paint <br>
-            5. Decorate your table with the ${decor} <br>`;
+            // myText =
+            //     `Estimated time to build the table: ${aTime} <br>
+            // <br>
+            // Materials Needed:<br>
+            // 1 square piece of ${adj} wood <br>
+            // 4 table legs <br>
+            // ${aNum} nails <br>
+            // ${color} paint <br>
+            // 1 ${decor} <br>
+            // <br>
+            // Instructions: <br>
+            // 1. Put the square piece of wood ${direction} on the ground <br>
+            // 2. Nail each leg to the corners of the square wood using a ${tool} <br>
+            // 3. Sand the table with a piece of ${item} <br>
+            // 4. Paint the table with your ${color} paint <br>
+            // 5. Decorate your table with the ${decor} <br>`;
         } else {
-            myText = "Please complete the form so I can make your Mad Lib!";
+            // myText = "Please complete the form so I can make your Mad Lib!";
         }
 
-        madLib.innerHTML = myText;
+        // madLib.innerHTML = myText;
 
         // reset the fields
         // let aTime = document.querySelector("#aTime").value  = "";
