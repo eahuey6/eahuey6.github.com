c16ace8ee07203721267a5eda109f05d18286b3e
eahuey6 <evelynhuey@hotmail.com>
2022-03-07 01:50:48 -0800

    v3 card overlay workinggit add .

--- des157b/final/version3/script.js
+++ des157b/final/version3/script.js
@@ -13,7 +13,6 @@
 
 
     let persons = document.getElementsByName("person");
-    let chosenPerson;
 
 
     const stripes = document.getElementById('stripes');
@@ -38,15 +37,22 @@
     const galleryBtnThank = document.getElementById('goGallery-btn');
     const entryBtnThank = document.getElementById('entry-btn');
 
+
+    // card page 
+    const cardContainer = document.querySelector('#card-overlay-container');
+
     
     
     // ------------------NAV BAR CODE------------------------
+    
     logoBtn.addEventListener('click', function(){
         landingPage.className = 'showing';
         galleryPage.className = 'hidden';
         thankPage.className = 'hidden';
         formPage.className = 'hidden';
         cardPage.className = 'hidden';
+        affirmationNav.style.textDecoration = 'none';
+        galleryNav.style.textDecoration = 'none';
     })
 
     affirmationNav.addEventListener('click', function(){
@@ -55,6 +61,8 @@
         thankPage.className = 'hidden';
         formPage.className = 'showing';
         cardPage.className = 'hidden';
+        affirmationNav.style.textDecoration = 'underline';
+        galleryNav.style.textDecoration = 'none';
     })
 
     galleryNav.addEventListener('click', function(){
@@ -63,6 +71,8 @@
         thankPage.className = 'hidden';
         formPage.className = 'hidden';
         cardPage.className = 'hidden';
+        affirmationNav.style.textDecoration = 'none';
+        galleryNav.style.textDecoration = 'underline';
         startGalleryPage()
         
     })
@@ -74,11 +84,15 @@
     affirmationBtnHome.addEventListener('click', function(){
         landingPage.className = 'hidden';
         formPage.className = 'showing';
+        affirmationNav.style.textDecoration = 'underline';
+        galleryNav.style.textDecoration = 'none';
     })
 
     galleryBtnHome.addEventListener('click', function(){
         landingPage.className = 'hidden';
         galleryPage.className = 'showing';
+        affirmationNav.style.textDecoration = 'none';
+        galleryNav.style.textDecoration = 'underline';
         startGalleryPage()
     })
 
@@ -152,10 +166,14 @@
     galleryBtnThank.addEventListener('click', function(){
         galleryPage.className = 'showing';
         thankPage.className = 'hidden';
+        affirmationNav.style.textDecoration = 'none';
+        galleryNav.style.textDecoration = 'underline';
     })
     entryBtnThank.addEventListener('click', function(){
         formPage.className = 'hidden';
         thankPage.className = 'hidden';
+        affirmationNav.style.textDecoration = 'none';
+        galleryNav.style.textDecoration = 'none';
     })
 
 
@@ -178,15 +196,16 @@
                 let cardText;
                 
 
-                if (text.length > 230){
-                    const slicedText = text.slice(0, 230);
+                if (text.length > 150){
+                    const slicedText = text.slice(0, 150);
                     cardText = `${slicedText}...`
 
                 } else {
                     cardText = text;
                     
                 }
-
+                // const addMoreBtn = '<button class="moreBtn">more</button>';
+                // addMoreBtn.setAttribute('id', `r-${id}`);
                 const theArticle = document.createElement('article');
                 theArticle.setAttribute('id', `r-${id}`);
                 theArticle.className = 'gallery-card';
@@ -194,22 +213,25 @@
                 <section class="card-content">
                     <h3 class="card-address">Dear <br><span class="card-name">${address}</span>, </h3>
                     <p class="card-text">${cardText}</p>
-                    <button class="seeMoreBtn">see more</button>
+                    <button id="e-${id}" class="moreBtn">more</button>
                 </section>
                     `;
                 gallerySection.append(theArticle);
             });
 
-
-            // styles the responsive stripe height on the page
-            let pageHeight = document.body.clientHeight;
-            stripes.style.height = `${pageHeight}px`;
-            console.log(pageHeight);
+            setStripes();
 
         } catch (error) {
             console.error('Error while fetching Affirmations', error);
         }
         
+    }
+
+    function setStripes(){
+        // styles the responsive stripe height on the page
+        let pageHeight = document.body.clientHeight;
+        stripes.style.height = `${pageHeight}px`;
+        console.log(pageHeight);
     }
 
     function startGalleryPage(){
@@ -221,8 +243,65 @@
             console.error('Error while creating Affirmation', error);
         }
     }
-    
 
+
+    // code for individual gallery card
+    document.addEventListener('click', function(event){
+        if (event.target.matches('.moreBtn')){
+            const thisRecord = event.target.getAttribute('id').slice(2);
+            setCard(thisRecord)
+            galleryPage.className = 'hidden';
+            cardPage.className = 'showing';
+        }
+    }, false);
+    
+    async function setCard(recordId){
+        const theAffirmations = Parse.Object.extend('Affirmations');
+        const query = new Parse.Query(theAffirmations);
+        query.equalTo('objectId', recordId);
+
+        try{
+            const results = await query.find();
+            results.forEach(function(thisFriend){
+                const thisAddress = thisFriend.get('address');
+                const thisText = thisFriend.get('text');
+                const thisPerson = thisFriend.get('person');
+
+    
+                cardContainer.innerHTML = '';
+                const overlaySection = document.createElement('article');
+                // overlaySection.setAttribute('id', `o-${id}`);
+                overlaySection.innerHTML = `
+                <section id="card-overlay-content">
+                            <h3 class="card-address">Dear <br><span class="card-name">${thisAddress}</span>, </h3>
+                            <p class="card-text">${thisText}</p>
+                </section>
+                    `;
+                cardContainer.append(overlaySection);
+                console.log('success');
+                
+                // const cardContainer = document.querySelector('#card-overlay-container');
+                // cardContainer.innerHTML = '';
+                // const overlaySection = document.createElement('section');
+                // overlaySection.setAttribute('id', `o-${id}`);
+                // overlaySection.className = 'gallery-card';
+                // overlaySection.innerHTML = `
+                // <section id="card-overlay-content">
+                //             <h3 class="card-address">Dear <br><span class="card-name">${thisAddress}</span>, </h3>
+                //             <p class="card-text">${thisText}</p>
+                // </section>
+                //     `;
+                // cardContainer.append(overlaySection);
+
+            })
+        } catch {
+            console.error('error while fetching affirmations', error);
+
+        }
+
+        
+        
+    }
 
 
 
